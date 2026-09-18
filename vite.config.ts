ac59238c9de3ebb03650122acd560aac1b85c2f1
Guilherme <gsfranzoni@gmail.com>
2026-09-18 17:33:44 -0300

    feat: first commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const getBase = () => {
+  const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+
+  if (process.env.VITE_BASE_PATH) {
+    return `${process.env.VITE_BASE_PATH.replace(/\/+$/, "")}/`;
+  }
+
+  if (process.env.GITHUB_ACTIONS && repositoryName) {
+    return `/${repositoryName}/`;
+  }
+
+  return "/";
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: getBase(),
+  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
