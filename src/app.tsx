8a2ce2168166c0fd7ac533c2dd2cfd1e14e3b23f
Guilherme <gsfranzoni@gmail.com>
2026-09-18 18:19:27 -0300

    feat: add canvas component and integrate d3 for visualization

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
+import { Canvas, type Line, type Point } from "./components/canvas";
+
 export function App() {
-  return <></>;
+  const [points, setPoints] = useState<Point[]>([{ x: 0, y: 0 }]);
+
+  const [lines, setLines] = useState<Line[]>([{ from: { x: 0, y: 0 }, to: { x: 1, y: 1 } }]);
+
+  return (
+    <main className="flex h-svh w-svw items-center justify-center">
+      <Canvas
+        points={points}
+        lines={lines}
+        onClick={(point) => {
+          setPoints((prev) => [...prev, point]);
+        }}
+      />
+    </main>
+  );
 }
